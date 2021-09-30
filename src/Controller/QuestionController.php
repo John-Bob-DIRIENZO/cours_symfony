1f32d2034e20ca4088358c9108fa74d7db6fbc86
John-Bob-DIRIENZO <jf.dirienzo@gmail.com>
2021-09-30 22:53:38 +0200

    un truc

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,10 +2,11 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class QuestionController
+class QuestionController extends AbstractController
 {
     /**
      * @Route("/")
@@ -21,10 +22,16 @@ class QuestionController
      */
     public function show($ma_wildcard)
     {
-        return new Response(sprintf(
-            'La question posée est : %s',
-            $ma_wildcard
-        ));
+        $answers = [
+            'Je ne suis pas spécialement magicien moi !',
+            'As tu essayé de fermer les fenêtres et de recommencer ?',
+            'Crame tout !'
+        ];
+
+        return $this->render('question/show.html.twig', [
+            'question' => sprintf('La question posée est : %s', $ma_wildcard),
+            'answers' => $answers
+        ]);
     }
 }
 
