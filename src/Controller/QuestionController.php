7c596d07b8c21f7b43a24b700e2b38a04fc0843b
John-Bob-DIRIENZO <jf.dirienzo@gmail.com>
2021-10-02 18:14:59 +0200

    avant sentry

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Service\MarkdownHelper;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -20,19 +21,22 @@ class QuestionController extends AbstractController
     /**
      * @Route("/questions/{ma_wildcard}", name="app_show")
      */
-    public function show($ma_wildcard)
+    public function show($ma_wildcard, MarkdownHelper $helper)
     {
+        $question_text = "Ma pizza finalement **ne convient pas** à mon intérieur, est-il possible de la retourner au magasin ?";
+
+        $parsedQuestion = $helper->parse($question_text);
+
         $answers = [
             'Je ne suis pas spécialement magicien moi !',
             'As tu essayé de fermer les fenêtres et de recommencer ?',
             'Crame tout !'
         ];
 
-        dump($this);
-
         return $this->render('question/show.html.twig', [
             'question' => sprintf('La question posée est : %s', $ma_wildcard),
-            'answers' => $answers
+            'answers' => $answers,
+            'question_text' => $parsedQuestion
         ]);
     }
 }
