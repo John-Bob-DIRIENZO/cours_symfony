f8ac1a2722a1872a34981b4bf13ba9d589c7f30f
John-Bob-DIRIENZO <jf.dirienzo@gmail.com>
2021-10-03 19:50:57 +0200

    le cours sur doctrine

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Question;
 use App\Service\MarkdownHelper;
+use Doctrine\ORM\EntityManagerInterface;
 use Sentry\State\HubInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -17,6 +19,35 @@ class QuestionController extends AbstractController
     public function homepage()
     {
         return $this->render('question/homepage.html.twig', []);
+    }
+
+    /**
+     * @Route("/questions/new")
+     */
+    public function new(EntityManagerInterface $entityManager)
+    {
+        $question = new Question();
+        $question->setName('Comment rendre une pizza ?')
+            ->setSlug('comment-rendre-une-pizza' . rand(0, 1000))
+            ->setQuestion(<<<EOF
+'Ma pizza finalement ne convient pas à mon intérieur, 
+est-il possible de la retourner au magasin ?'
+EOF
+            );
+
+        if (rand(1, 10) > 2) {
+            $question->setAskedAt(new \DateTime(sprintf('-%d days', rand(1, 100))));
+        }
+
+        // Il faut faire les deux pour que ça marche
+        $entityManager->persist($question); // Informe doctrine de l'objet, je peux en avoir plusieurs
+        $entityManager->flush(); // Fait la query
+
+        return new Response(sprintf('Votre question %s et avec l\'id %d est instrit en BDD',
+                $question->getSlug(),
+                $question->getId() // Je récupère l'id après la sauvegarde
+            )
+        );
     }
 
     /**
